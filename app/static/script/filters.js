34c4696a69dd1812e99a646c7b3cd6cfd01ccc2f
Messai <messai@github.com>
2023-08-30 10:54:13 +0300

    style changes and some dynamic content addition like scroll bars

--- app/static/script/filters.js
+++ app/static/script/filters.js
@@ -1,23 +1,36 @@
 $('document').ready( function() {
-  let filters = [];
+  let filters = {};
   $('.checkoption').click(function() {
     $('.checkoption').not(this).prop('checked', false);
-    filters.push($(this).attr('data-name'))
+    filters[$(this).attr('data-name')] = $(this).attr('data-value')
   });
 
   $('.locationoption').click(function() {
     $('.locationoption').not(this).prop('checked', false)
-    filters.push($(this).attr('data-name'))
+    filters[$(this).attr('data-name')] = $(this).attr('data-value')
   });
 
   $('.regionoption').click(function() {
     $('.regionoption').not(this).prop('checked', false);
-    filters.push($(this).attr('data-name'))
+    filters[$(this).attr('data-name')] = $(this).attr('data-value')
   });
 
   $('.weatheroption').click(function() {
     $('.weatheroption').not(this).prop('checked', false);
-    filters.push($(this).attr('data-name'))
+    filters[$(this).attr('data-name')] = $(this).attr('data-value')
   });
 	console.log(filters)
+
+  $.getJSON('static/file.json', function (data) {
+    const info = data;
+    for (i in info) {
+      if (info[i]['__class__'] == 'City') {
+	let name = info[i]['name']
+        if (filters['weather'] == info[i]['weather']) {
+	  $('#results').append('<li>'+ name + '</li>');
+		console.log(info[i][name])
+	}
+      }
+    }
+  });
 });
