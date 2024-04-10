c9ce6092ba9fa99852e13b48e91d95eb9abccc7b
Messai <messai@github.com>
2024-04-11 01:24:15 +0300

    some optimization

--- app/app.py
+++ app/app.py
@@ -1,6 +1,7 @@
-from flask import Flask, render_template, request, jsonify
 import json
 import requests
+from flask import Flask, render_template, request, jsonify
+from connection import baseUrl
 
 app = Flask(__name__)
 
@@ -11,22 +12,20 @@ def index():
     if request.args:
         city = request.args.get('city_name')
         if city:
-            response = requests.get('http://0.0.0.0:5001/api/v1/all')
+            response = requests.get(baseUrl + '/all')
             data = response.json()
             
             if response.status_code == 404:
                 return
 
             for i in data:
-                if i['__class__'] == 'City':
-                    if i['name'] == city:
+                if i['__class__'] == 'City' and i['name'] == city:
                         region = i['region']
                         weather = i['weather']
                         population = i['population']
                         ids = i['id']
                         for j in data:
-                            if j['__class__'] == 'TouristSite':
-                                if j['city_id'] == ids:
+                            if j['__class__'] == 'TouristSite' and j['city_id'] == ids:
                                     places.append(j['name'])
 
                         return render_template("city_specific.html",
@@ -36,82 +35,51 @@ def index():
                                                population=population,
                                                ids=ids,
                                                places=places)
-                    else:
-                        pass
-                else:
-                    pass
             return jsonify({'error': "not found"})
-        else:
-            return jsonify({'error': 'not found'})
-
-    else:
-        return render_template("index.html")
+    return render_template("index.html")
 
 @app.route("/<city>", methods=["GET"], strict_slashes=False)
 def get_city_info(city):
     places = []
     if city:
-        response = requests.get('http://0.0.0.0:5001/api/v1/all')
-        
+        response = requests.get(baseUrl + '/all')
         data = response.json()
 
         if data: 
             for i in data:
-                if i['__class__'] == 'City':
-                    if i['name'] == city: 
-                        region = i['region']
-                        weather = i['weather']
-                        population = i['population']
-                        ids = i['id']
-                        for j in data:
-                                if j['__class__'] == 'TouristSite':
-                                    if j['city_id'] == ids:
-                                        places.append(j['name'])
-
-                        return render_template("city_specific.html", 
+                if i['__class__'] == 'City' and i['name'] == city: 
+                    region = i['region']
+                    weather = i['weather']
+                    population = i['population']
+                    ids = i['id']
+                    for j in data:
+                        if j['__class__'] == 'TouristSite' and j['city_id'] == ids:
+                            places.append(j['name'])
+                    return render_template("city_specific.html", 
                                                 city=city,
                                                 region=region,
                                                 weather=weather,
                                                 population=population,
                                                 ids=ids,
                                                 places=places)
-                    else:
-                        pass
-                    
-            else:
-                pass
         return jsonify({'error': 'not found'})
-
-    else:
-        return jsonify({'error': 'not found'})
+    return jsonify({'error': 'not found'})
 
 
 @app.route("/<city>/<site>", methods=["GET"])
 def getsiteinfo(city, site):
     if city and site:
-        response = requests.get('http://0.0.0.0:5001/api/v1/all')
+        response = requests.get(baseUrl + '/all')
         data = response.json()
         for i in data:
-            if i['__class__'] == 'City':
-                if i['name'] == city:
-                    for j in data:
-                        if j['__class__'] == 'TouristSite':
-                            if j['name'] == site:
-                                return render_template('places_specific.html',
-                                                        site=site,
-                                                        city=city)
-                            else:
-                                pass
-                        else:
-                            pass
-                else:
-                    pass
-            else:
-                pass
+            if i['__class__'] == 'City' and i['name'] == city:
+                for j in data:
+                    if j['__class__'] == 'TouristSite' and j['name'] == site:
+                            return render_template('places_specific.html',
+                                                    site=site,
+                                                    city=city)
         return jsonify({'error': 'not found'})
-
-    else:
-        return jsonify({'error': 'not found'})
+    return jsonify({'error': 'not found'})
 
 
 @app.route("/contribute", methods=["GET"])
@@ -121,7 +89,7 @@ def contribute():
 
 @app.route('/add_city', methods=["GET"])
 def add_city():
-    requests.post('http://0.0.0.0:5001', headers={})
+    requests.post(baseUrl, headers={})
     return render_template("index.html")
 
 
